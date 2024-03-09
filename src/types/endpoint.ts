d4b66c31509053442bbf7465c1eeb0e4a0493912
dinohan <dinohan.dev@gmail.com>
2024-03-09 12:00:46 +0900

    style(endpoint.ts): fix indentation

--- src/types/endpoint.ts
+++ src/types/endpoint.ts
@@ -3,13 +3,13 @@ import { Method } from "./method";
 type value = any;
 
 export type Endpoint<
-    Parameter extends Record<string, value> | string,
-    Response extends Record<string, value> | string
+  Parameter extends Record<string, value> | string,
+  Response extends Record<string, value> | string
 > = {
-    method: Method;
-    path: string | ((e: Parameter) => string);
-    pathParams?: (keyof Parameter)[];
-    queryParams?: (keyof Parameter)[];
-    bodyParams?: (keyof Parameter)[];
-    headers?: Record<string, string>;
+  method: Method;
+  path: string | ((e: Parameter) => string);
+  pathParams?: (keyof Parameter)[];
+  queryParams?: (keyof Parameter)[];
+  bodyParams?: (keyof Parameter)[];
+  headers?: Record<string, string>;
 };
