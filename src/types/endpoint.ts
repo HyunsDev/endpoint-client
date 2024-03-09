cbaee08945027ac54a64e730d60795e113380972
dinohan <dinohan.dev@gmail.com>
2024-03-09 12:07:56 +0900

    feat: add utility types for path parameters

--- src/types/endpoint.ts
+++ src/types/endpoint.ts
@@ -1,4 +1,5 @@
 import { Method } from "./method";
+import { ExtractParams, PathParamType } from "./utility.types";
 
 type value = any;
 
@@ -12,4 +13,25 @@ export type Endpoint<
   queryParams?: (keyof Parameter)[];
   bodyParams?: (keyof Parameter)[];
   headers?: Record<string, string>;
+};
+
+/**
+ * Forces the path parameter defined in path string to be specified.
+ *
+ * @example
+ * type Path = "/user/:id";
+ * type Endpoint = __Experimental__Endpoint<"GET", "/user/:id">;
+ * const endpoint: Endpoint = {
+ *   method: "GET",
+ *   path: "/user/:id",
+ *   pathParams: {}
+ * // ^^^^^^^^^ Error: Property 'id' is missing in type '{}' but required in type 'Record<"id", PathParamType>'.
+ * }
+ */
+export type __Experimental__Endpoint<M extends Method, P extends string> = {
+  method: M;
+  path: P;
+  pathParams: ExtractParams<P> extends never
+    ? never
+    : Record<ExtractParams<P>, PathParamType>;
 };
